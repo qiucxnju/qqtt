361766eb26a468c01b3cbb6ed651c34766f78512
qq <qiucxnju@gmail.com>
2020-11-30 15:53:11 +0800

    管理界面优化blog显示

--- website/qtblog/admin.py
+++ website/qtblog/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from qtblog.models import Tag
 from qtblog.models import Blog
-
+class BlogAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'path', 'date', 'modify', 'state')
+    list_display = ('title', 'path', 'date', 'modify', 'state')
+    
 admin.site.register(Tag)
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin)
 # Register your models here.
