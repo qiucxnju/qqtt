632c57c77e09755eefd421fc4e12652b8674343a
qiucxnju <qiucxnju@gmail.com>
2021-05-11 16:50:06 +0800

    成功追踪，但是505错误太多

--- android/app/src/main/java/com/qqtt/android/BackService.java
+++ android/app/src/main/java/com/qqtt/android/BackService.java
@@ -26,26 +26,41 @@ import java.util.TimerTask;
 
 public class BackService extends Service {
     String base_url = "http://www.qqttgroup.com/findpapa/setLocation";
-    public void sendLocation(String cookie, double lng, double lat) {
-        HttpURLConnection conn=null;
-        try {
-        URL url = new URL(base_url + "?lng=" + lng + "&lat=" + lat);
-         conn = (HttpURLConnection)url.openConnection();
-        conn.setRequestProperty("Cookie", cookie);
+    public  class  SendThread  extends  Thread
+    {
+        HttpURLConnection conn = null;
+        public  String cookie;
+        public double lng, lat;
+        public  SendThread(String cookie, double lng, double lat)
+        {
+            this .cookie = cookie;
+            this.lng = lng;
+            this.lat = lat;
+        }
+        public  void  run()
+        {
+            try {
+            URL url = new URL(base_url + "?lng=" + lng + "&lat=" + lat);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("Cookie", cookie);
             conn.setConnectTimeout(5000);
             conn.setRequestMethod("GET");
-        if (HttpURLConnection.HTTP_OK == conn.getResponseCode()) {
-            System.out.println("发送成功");
-        }else{
-            System.out.println("发送失败");
-        }
+            if (HttpURLConnection.HTTP_OK == conn.getResponseCode()) {
+                System.out.println("发送成功");
+            } else {
+                System.out.println("发送失败");
+            }
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             conn.disconnect();
         }
-        return ;
+            return;
+        }
+    }
+    public void sendLocation(String cookie, double lng, double lat) {
+        Thread thread = new SendThread(cookie,lng, lat);
+        thread.start();
     }
     public String getCookie(){
         System.out.println("timer");
@@ -79,81 +94,66 @@ public class BackService extends Service {
             return null;
         }
     }
+    LocationClient locationClient;
+    private void initLocationOption() {
+        System.out.println("init location option");
+//定位服务的客户端。宿主程序在客户端声明此类，并调用，目前只支持在主线程中启动
+        locationClient = new LocationClient(getApplicationContext());
+//声明LocationClient类实例并配置定位参数
+        LocationClientOption locationOption = new LocationClientOption();
+        MyLocationListener myLocationListener = new MyLocationListener();
+//注册监听函数
+        locationClient.registerLocationListener(myLocationListener);
+//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        locationOption.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
+        locationOption.setCoorType("BD09LL");
+//可选，默认0，即仅定位一次，设置发起连续定位请求的间隔需要大于等于1000ms才是有效的
+        locationOption.setScanSpan(5 * 60 * 1000);
+//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        locationOption.setIgnoreKillProcess(true);
+//可选，默认false，设置是否开启Gps定位
+        locationOption.setOpenGps(true);
+//需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
+        locationClient.setLocOption(locationOption);
+//开始定位
+        locationClient.start();
+    }
+    class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location){
+            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+            //以下只列举部分获取经纬度相关（常用）的结果信息
+            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+
+            //获取纬度信息
+            double latitude = location.getLatitude();
+            //获取经度信息
+            double longitude = location.getLongitude();
+            //获取定位精度，默认值为0.0f
+            float radius = location.getRadius();
+            //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
+            String coorType = location.getCoorType();
+            //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
+            int errorCode = location.getLocType();
+            System.out.println(errorCode);
+
+            String cookie = getCookie();
+
+
+            System.out.println(longitude);
+            System.out.println(latitude);
+            if((longitude > 50)&&(longitude < 200))
+                sendLocation(cookie, longitude, latitude);
+        }
+    }
     Timer timer = new Timer();
     TimerTask task = new TimerTask() {
 
-        class MyLocationListener extends BDAbstractLocationListener {
-            @Override
-            public void onReceiveLocation(BDLocation location){
-                //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
-                //以下只列举部分获取经纬度相关（常用）的结果信息
-                //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
 
-                //获取纬度信息
-                double latitude = location.getLatitude();
-                //获取经度信息
-                double longitude = location.getLongitude();
-                //获取定位精度，默认值为0.0f
-                float radius = location.getRadius();
-                //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
-                String coorType = location.getCoorType();
-                //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
-                int errorCode = location.getLocType();
-
-                String cookie = getCookie();
-
-
-                System.out.println(longitude);
-                System.out.println(latitude);
-                //sendLocation(cookie, longitude, latitude);
-            }
-        }
-        private void initLocationOption() {
-//定位服务的客户端。宿主程序在客户端声明此类，并调用，目前只支持在主线程中启动
-            LocationClient locationClient = new LocationClient(getApplicationContext());
-//声明LocationClient类实例并配置定位参数
-            LocationClientOption locationOption = new LocationClientOption();
-            MyLocationListener myLocationListener = new MyLocationListener();
-//注册监听函数
-            locationClient.registerLocationListener(myLocationListener);
-//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
-            locationOption.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
-//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
-            locationOption.setCoorType("gcj02");
-//可选，默认0，即仅定位一次，设置发起连续定位请求的间隔需要大于等于1000ms才是有效的
-            locationOption.setScanSpan(1000);
-//可选，设置是否需要地址信息，默认不需要
-            locationOption.setIsNeedAddress(true);
-//可选，设置是否需要地址描述
-            locationOption.setIsNeedLocationDescribe(true);
-//可选，设置是否需要设备方向结果
-            locationOption.setNeedDeviceDirect(false);
-//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
-            locationOption.setLocationNotify(true);
-//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
-            locationOption.setIgnoreKillProcess(true);
-//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
-            locationOption.setIsNeedLocationDescribe(true);
-//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
-            locationOption.setIsNeedLocationPoiList(true);
-//可选，默认false，设置是否收集CRASH信息，默认收集
-            locationOption.SetIgnoreCacheException(false);
-//可选，默认false，设置是否开启Gps定位
-            locationOption.setOpenGps(true);
-//可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
-            locationOption.setIsNeedAltitude(false);
-//设置打开自动回调位置模式，该开关打开后，期间只要定位SDK检测到位置变化就会主动回调给开发者，该模式下开发者无需再关心定位间隔是多少，定位SDK本身发现位置变化就会及时回调给开发者
-            locationOption.setOpenAutoNotifyMode();
-//设置打开自动回调位置模式，该开关打开后，期间只要定位SDK检测到位置变化就会主动回调给开发者
-            locationOption.setOpenAutoNotifyMode(3000,1, LocationClientOption.LOC_SENSITIVITY_HIGHT);
-//需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
-            locationClient.setLocOption(locationOption);
-//开始定位
-            locationClient.start();
-        }
         @Override
         public void run(){
-            //initLocationOption();
+            initLocationOption();/*
             String cookie = getCookie();
 
 
@@ -164,7 +164,7 @@ public class BackService extends Service {
             double lng = location.getLongitude();
             System.out.println(lat);
             System.out.println(lng);
-            sendLocation(cookie, lng, lat);/*
+            sendLocation(cookie, lng, lat);
 */
 
         }
@@ -197,7 +197,7 @@ public class BackService extends Service {
             return 0;
         }
         System.out.println("service start");
-        timer.schedule(task, 0, 5 * 1000);
+        initLocationOption();
 
         return START_NOT_STICKY;//START_STICKY;
     }
@@ -223,7 +223,7 @@ public class BackService extends Service {
     /** 服务不再有用且将要被销毁时调用 */
     @Override
     public void onDestroy() {
-        timer.cancel();
+        locationClient.stop();
         System.out.println("service destroy");
         /*
         Intent alarm_intent = new Intent();
